418dbce724793bccb927c465e48561c065339cde
VincentEichenseher <65071706+VincentEichenseher@users.noreply.github.com>
2024-03-06 19:35:40 +0100

    reuploaded final files

---
+++ python/CustomNodes.py
@@ -0,0 +1,282 @@
+import numpy as np
+import pandas as pd
+
+from pyqtgraph import BarGraphItem, PlotDataItem, ScatterPlotItem
+from pyqtgraph.flowchart.library.common import CtrlNode
+from pyqtgraph.Qt import QtCore
+
+class SelectColumnNode(CtrlNode):
+    "returns column data by name"
+    nodeName = 'GetColumnData'
+    uiTemplate = [
+        ('column',  'intSpin', {'value': 0, 'step': 1, 'bounds': [0, None]}),]
+    
+    def __init__(self, name):
+        terminals={'data': {'io':'in'},'output':{'io':'out'}}
+        CtrlNode.__init__(self, name, terminals=terminals)
+        
+        
+    def process(self, **kwds):
+        index = self.ctrls['column'].value()
+        df = pd.DataFrame.from_dict(kwds['data'])
+        column = list(df.keys())[index]
+        data = df[column]
+        result = data.to_dict()
+        self.rename(str(column)+" Data")
+        self.graphicsItem().setPen()
+        self.graphicsItem().nameItem.setPlainText(self._name)
+        return {'output': result}
+    
+class JoinDataNode(CtrlNode):
+    "preforms join on dataframes or columns"
+    nodeName = 'JoinData'
+    uiTemplate = [
+        ('join_type',  'combo', {'values': ["Full","Inner","Right","Left"], 'index': 0}),]
+    
+    def __init__(self, name):
+        terminals={'A': {'io':'in'},'B':{'io':'in'},'output':{'io':'out'}}
+        CtrlNode.__init__(self, name, terminals=terminals)
+        
+        
+    def process(self, **kwds):
+        joinType = str(self.ctrls['join_type'].currentText())
+        self.rename(joinType +" Join")
+        self.graphicsItem().setPen()
+        self.graphicsItem().nameItem.setPlainText(self._name)
+        try:
+            dfA = pd.DataFrame.from_dict(kwds['A'], orient='columns')
+            dfB = pd.DataFrame.from_dict(kwds['B'], orient='columns')
+        except:
+            dfA = pd.DataFrame.from_dict(kwds['A'], orient='index')
+            dfB = pd.DataFrame.from_dict(kwds['B'], orient='index')
+        if joinType == "Full":
+            data = dfA.join(dfB, how="outer", lsuffix="_A", rsuffix="_B")
+        elif joinType == "Inner":
+            data = dfA.join(dfB, how="inner", lsuffix="_A", rsuffix="_B")
+        elif joinType == "Left":
+            data = dfA.join(dfB, how="left", lsuffix="_A", rsuffix="_B")
+        elif joinType == "Right":
+            data = dfA.join(dfB, how="right", lsuffix="_A", rsuffix="_B")
+        result = data.to_dict()
+        return {'output': result}
+    
+class ThresholdDataNode(CtrlNode):
+    "preforms clip on dataframes or columns (applies threshold)"
+    nodeName = 'ThresholdData'
+    uiTemplate = [
+        ('lower_threshold', 'spin', {'value': 0.0, 'step': 1, 'bounds': [0.0, None]}),
+        ('upper_threshold', 'spin', {'value': None, 'step': 1, 'bounds': [0.0, None]})]
+    
+    def __init__(self, name):
+        terminals={'data':{'io':'in'},'output':{'io':'out'}}
+        CtrlNode.__init__(self, name, terminals=terminals)
+        
+        
+    def process(self, **kwds):
+        thresLower = self.ctrls["lower_threshold"].value()
+        thresUpper = self.ctrls["upper_threshold"].value()
+        self.rename(str(thresLower) + " ≤ Data ≤ " + str(thresUpper))
+        self.graphicsItem().setPen()
+        self.graphicsItem().nameItem.setPlainText(self._name)
+        try:
+            df = pd.DataFrame.from_dict(kwds['data'])
+        except:
+            df = pd.DataFrame.from_dict(kwds['data'],orient='index')
+        data = df.clip(thresLower,thresUpper)
+        result = data.to_dict()
+        return {'output': result}
+    
+
+class GroupByNode(CtrlNode):
+    "groups a variable by another variable and calculates statistics"
+    nodeName = 'CalculateStats'
+    uiTemplate = [
+        ('column',  'intSpin', {'value': 0, 'step': 1, 'bounds': [0, None]}),
+        ('group_by',  'intSpin', {'value': 0, 'step': 1, 'bounds': [0, None]}),]
+    
+    def __init__(self, name):
+        terminals = {'data': {'io':'in'},
+                     'min':{'io':'out'},
+                     'max':{'io':'out'},
+                     'mean':{'io':'out'},
+                     'median':{'io':'out'},
+                     'var':{'io':'out'},
+                     'std':{'io':'out'},
+                     'group':{'io':'out'}}
+        CtrlNode.__init__(self, name, terminals=terminals)
+        
+    def process(self, **kwds):
+        df = pd.DataFrame.from_dict(kwds['data'])
+        index = list(df.keys())[self.ctrls['column'].value()]
+        group_var = None
+        if self.ctrls['group_by'].value != None:
+            group_var = list(df.keys())[self.ctrls['group_by'].value()]
+            stats = df.groupby(group_var).agg(['min','max','mean','median','std','var'])
+        else:
+            stats = df.agg(['min','max','mean','median','std','var'])
+            stats = stats.unstack(level=-1).to_frame(0).T
+        stats = stats.set_axis(stats.columns.map('_'.join), axis=1, inplace=False)
+        result = stats.to_dict()
+        self.rename(str(index)+" by " + str(group_var))
+        self.graphicsItem().setPen()
+        self.graphicsItem().nameItem.setPlainText(self._name)
+        if self.ctrls['column'].value() != self.ctrls['group_by'].value():
+            return {'max': result[str(index) + '_' + 'max'],
+                    'min': result[str(index)+ '_' + 'min'],
+                    'mean': result[str(index)+ '_' + 'mean'],
+                    'var': result[str(index)+ '_' + 'var'],
+                    'std': result[str(index)+ '_' + 'std'],
+                    'group': df[group_var].to_dict()}
+        else:
+            return {'group': None}
+        
+class MathOperationNode(CtrlNode):
+    "preforms a mathemetical operation (+, -, *, /) on dataframes or columns"
+    nodeName = 'MathOperation'
+    uiTemplate = [
+        ('operation_type',  'combo', {'values': ["Addition","Subtraction","Multiplication","Division"], 'index': 0}),]
+    
+    def __init__(self, name):
+        terminals={'A': {'io':'in'},'B':{'io':'in'},'output':{'io':'out'}}
+        CtrlNode.__init__(self, name, terminals=terminals)
+        
+        
+    def process(self, **kwds):
+        operationType = str(self.ctrls['operation_type'].currentText())
+        self.rename(operationType)
+        self.graphicsItem().setPen()
+        self.graphicsItem().nameItem.setPlainText(self._name)
+        try:
+            dfA = pd.DataFrame.from_dict(kwds['A'], orient='columns')
+            dfB = pd.DataFrame.from_dict(kwds['B'], orient='columns')
+        except:
+            dfA = pd.DataFrame.from_dict(kwds['A'], orient='index')
+            dfB = pd.DataFrame.from_dict(kwds['B'], orient='index')
+        if operationType == "Addition":
+            data = dfA + dfB
+        elif operationType == "Subtraction":
+            data = dfA - dfB
+        elif operationType == "Multiplication":
+            data = dfA * dfB
+        elif operationType == "Division":
+            data = dfA / dfB
+        result = data.to_dict()
+        return {'output': result}
+    
+class MissingDataNode(CtrlNode):
+    "imputates or drops missing values"
+    nodeName = 'MissingDataNode'
+    uiTemplate = [
+        ('handle_NAs',  'combo', {'values': ["imputate","drop"], 'index': 0}),
+        ('imputation_method',  'combo', {'values': ["mean","median","null"], 'index': 0})]
+    
+    def __init__(self, name):
+        terminals={'data':{'io':'in'},'output':{'io':'out'}}
+        CtrlNode.__init__(self, name, terminals=terminals)
+           
+    def process(self, **kwds):
+        approach = str(self.ctrls['handle_NAs'].currentText())
+        self.rename(approach + " missing values")
+        self.graphicsItem().setPen()
+        self.graphicsItem().nameItem.setPlainText(self._name)
+        try:
+            df = pd.DataFrame.from_dict(kwds['data'])
+        except:
+            df = pd.DataFrame.from_dict(kwds['data'],orient='index')
+        if  approach == "imputate":
+            imputate_meth = str(self.ctrls['imputation_method'].currentText())
+            if imputate_meth == "mean":
+                data = df.fillna(df.mean())
+            elif imputate_meth == "median":
+                data = df.fillna(df.median())
+            elif imputate_meth == "null":
+                data = df.fillna(0)
+        elif  approach == "drop":
+            data = df.dropna()
+        result = data.to_dict()
+        return {'output': result}
+    
+class LinePlotWidgetNode(CtrlNode):
+    "custom dual input PlotWidet"
+    nodeName = 'LinePlotWidget'
+    sigPlotChanged = QtCore.Signal(object)
+    uiTemplate = [
+        ('plot_type',  'combo', {'values': ["Scatterplot","Lineplot","Barchart"], 'index': 0}),]
+
+    def __init__(self, name):
+        terminals = {'X': {'io':'in'},'Y':{'io':'in'}}
+        CtrlNode.__init__(self, name, terminals=terminals)
+        self.plot = None
+
+    def setPlot(self, plot):
+        if plot == self.plot:
+            return
+        if self.plot is not None:
+             self.plot.clear()
+
+        self.plot = plot
+        self.update()
+        self.sigPlotChanged.emit(self)
+        
+    def process(self, X, Y, display=True):
+        plot_type = str(self.ctrls['plot_type'].currentText())
+        self.rename(plot_type + " Widget")
+        self.graphicsItem().setPen()
+        self.graphicsItem().nameItem.setPlainText(self._name)
+
+        if X and Y and display and self.plot is not None:
+            df1 = pd.DataFrame.from_dict(X,orient='index')
+            df2 = pd.DataFrame.from_dict(Y,orient='index')
+            if df1.empty == False and df2.empty == False:
+                xData = np.concatenate(df1.to_numpy(),axis=0).tolist()
+                yData = np.concatenate(df2.to_numpy(),axis=0).tolist()
+                self.plot.clear()
+                if plot_type == "Lineplot":
+                    plot = PlotDataItem(x=xData,y=yData)
+                elif plot_type == "Barchart":
+                    plot = BarGraphItem(x=xData, width=10/len(xData), height=yData)
+                elif plot_type == "Scatterplot":
+                    plot = ScatterPlotItem(xData, yData)
+                self.plot.addItem(plot)
+
+class HistogramWidgetNode(CtrlNode):
+    "custom single input PlotWidget"
+    nodeName = 'HistogramWidget'
+    sigPlotChanged = QtCore.Signal(object)
+    uiTemplate = [
+        ('plot_type',  'combo', {'values': ["Histogram","SimpleLineplot"], 'index': 0}),]
+
+    def __init__(self, name):
+        terminals = {'X':{'io':'in'}}
+        CtrlNode.__init__(self, name, terminals=terminals)
+        self.plot = None
+
+    def setPlot(self, plot):
+        if plot == self.plot:
+            return
+        if self.plot is not None:
+             self.plot.clear()
+
+        self.plot = plot
+        self.update()
+        self.sigPlotChanged.emit(self)
+        
+    def process(self, X, display=True):
+        plot_type = str(self.ctrls['plot_type'].currentText())
+        self.rename(plot_type + " Widget")
+        self.graphicsItem().setPen()
+        self.graphicsItem().nameItem.setPlainText(self._name)
+
+        if X and display and self.plot is not None:
+            df = pd.DataFrame.from_dict(X,orient='index')
+            if df.empty == False:
+                inputData = np.concatenate(df.to_numpy(),axis=0).tolist()
+                x_data = [x for x in inputData]
+                y_data = [inputData.count(x) for x in inputData]
+
+                self.plot.clear()
+                if plot_type == "SimpleLineplot":
+                    plot = PlotDataItem(inputData)
+                elif plot_type == "Histogram":
+                    plot = BarGraphItem(x=x_data, width=10/len(x_data), height=y_data)
+                self.plot.addItem(plot)
---
+++ python/Data_Flow_App.py
@@ -0,0 +1,188 @@
+#Based on the pyqtgraph Flowchart example that can be accessed with the command "pythom -m pyqtgraph.examples"
+import pandas as pd
+import pyqtgraph as pg
+import json
+import argparse
+import threading
+import sys
+
+from lib.pythonosc import dispatcher
+from lib.pythonosc import osc_server
+
+from parsers.MessageParser import MessageParser
+from parsers.MessageTypes import MessageTypes
+import signal
+
+from pyqtgraph.flowchart.NodeLibrary import NodeLibrary
+from pyqtgraph.flowchart import Flowchart
+from pyqtgraph.Qt import QtWidgets
+from pyqtgraph.Qt.QtCore import QTimer
+
+from CustomNodes import SelectColumnNode, JoinDataNode, ThresholdDataNode, GroupByNode, MathOperationNode, MissingDataNode, LinePlotWidgetNode, HistogramWidgetNode
+from sklearn import datasets
+
+# important params:
+# modify as needed
+update_interval = 500 # update interval in ms
+
+# temp storeage variables
+json_data = None #json data cache 
+elements_added = {} # dict to cache references to currently active elements (so these can be removed and to avoid duplication of elements on update)
+
+# create Qt app
+signal.signal(signal.SIGINT, signal.SIG_DFL) # SIGINT = Ctrl + C; this line lets us kill the Qt app and timer with te rest of this script
+app = QtWidgets.QApplication(sys.argv)
+
+## Create main window and layout
+win = QtWidgets.QMainWindow()
+win.setWindowTitle('pyqtgraph: Data-Flow graph')
+cw = QtWidgets.QWidget()
+win.setCentralWidget(cw)
+layout = QtWidgets.QGridLayout()
+cw.setLayout(layout)
+
+## Create flowchart object, set input/output Nodes (these are the input and output of the entire data-flow process)
+fc = Flowchart(terminals={
+    'dataIn': {'io': 'in'},
+    'dataOut': {'io': 'out'}    
+})
+w = fc.widget()
+w.chartWidget._viewBox.setMouseEnabled(False,False)
+## Add flowchart pannel
+layout.addWidget(w.chartWidget.viewDock, 0, 0, 2, 1)
+
+## init two plot widgets (areas for displaying plots) to main screen
+pw1 = pg.PlotWidget()
+pw2 = pg.PlotWidget()
+layout.addWidget(pw1, 0, 1, 1, 1)
+layout.addWidget(pw2, 1, 1, 1, 1)
+
+layout.setColumnStretch(0, 27)
+layout.setColumnStretch(1, 15)
+
+win.showMaximized()
+
+# load data
+df = pd.read_csv("./rice_beef_coffee_price_changes.csv")
+data = df.to_dict()
+
+## set Input Node data
+fc.setInput(dataIn=data)
+                        
+# add custom Nodes
+
+nodeLibrary = NodeLibrary()
+nodeLibrary.addNodeType(SelectColumnNode, [('Filter',)])
+nodeLibrary.addNodeType(JoinDataNode, [('Filter',)])
+nodeLibrary.addNodeType(ThresholdDataNode, [('Filter',)])
+nodeLibrary.addNodeType(GroupByNode, [('Filter',)])
+nodeLibrary.addNodeType(MathOperationNode, [('Operations',)])
+nodeLibrary.addNodeType(MissingDataNode, [('Operations',)])
+nodeLibrary.addNodeType(LinePlotWidgetNode, [('Display',)])
+nodeLibrary.addNodeType(HistogramWidgetNode, [('Display',)])
+fc.setLibrary(nodeLibrary)
+
+# set PlotWidget Nodes (Nodes for showing plots in the plotwidgets we created earlier)
+
+pw1Node = fc.createNode('LinePlotWidget', pos=(300, -100))
+pw1Node.setPlot(pw1)
+
+pw2Node = fc.createNode('HistogramWidget', pos=(300, -50))
+pw2Node.setPlot(pw2)
+
+# load json for the first time
+if json_data is None:
+    with open("../node_data.json","r") as file_in:
+        json_data = json.load(file_in)
+    for item in json_data["nodes"]:
+        item["is_active"] = False
+    with open("./node_data.json","w") as file_out:
+        file_out.write(json.dumps(json_data))
+    with open("./stack_data.json","w") as file_out_numerodos:
+        file_out_numerodos.write(json.dumps(json_data))
+
+# define functions
+        
+def main():
+    sys.setrecursionlimit(10000)
+    mp = MessageParser()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip", default="127.0.0.1", help="The ip to listen on")
+    parser.add_argument("--port", type=int, default=3333, help="The port to listen on")
+    args = parser.parse_args()
+
+    dispatch = dispatcher.Dispatcher()
+    
+    dispatch.map(MessageTypes.POINTER.value, mp.parse)
+    dispatch.map(MessageTypes.BOUNDS.value, mp.parse)
+    dispatch.map(MessageTypes.FRAME.value, mp.parse)
+    dispatch.map(MessageTypes.SYMBOL.value, mp.parse)
+    dispatch.map(MessageTypes.TOKEN.value, mp.parse)
+    dispatch.map(MessageTypes.ALIVE.value, mp.parse)
+    server = osc_server.BlockingOSCUDPServer((args.ip, args.port), dispatch)
+
+    print("Serving on {}".format(server.server_address))
+
+    server_ = threading.Thread(target=server.serve_forever)
+
+    #dw = QDrawWidget.QDrawWidget(app, mp)
+    #dw.on_close.connect(lambda: server.shutdown())
+
+    timer = QTimer()
+    timer.timeout.connect(create_nodes_from_json)
+
+    server_.start()
+
+    timer.start(update_interval)
+
+    app.aboutToQuit.connect(lambda: server.shutdown())
+    app.aboutToQuit.connect(lambda: timer.stop())
+
+    sys.exit(app.exec_())
+
+# create nodes based on changes to json
+def create_nodes_from_json():
+    global json_data
+    #listen for markers here
+    stack_data = None
+    try:
+        with open("./node_data.json","r") as fh:
+            json_data = json.load(fh)
+    except:
+#        print("cant open node_data")
+        pass
+    with open("./stack_data.json","r") as stack_fh:
+      stack_data = json.load(stack_fh)
+    for element in json_data["nodes"]:
+        el_id = element["aruco_id"]
+        id_as_string = str(el_id)
+        if element["is_active"] is True and id_as_string not in list(elements_added):
+            nodename = element["node_type"]
+            nodecoords = element["coord"]
+            node = fc.createNode(nodename, pos=[nodecoords[0], nodecoords[1]])
+            elements_added.update({id_as_string:node})
+        elif element["is_active"] is True and id_as_string in list(elements_added):
+            # change crtl-widget values
+            ui = elements_added[id_as_string].ctrls
+            graphics_item = elements_added[id_as_string].graphicsItem()
+            new_values = stack_data["nodes"][el_id]["values"]
+            new_coords = json_data["nodes"][el_id]["coord"]
+            graphics_item.setPos(new_coords[0],new_coords[1])
+            for key in new_values.keys():
+                if el_id in [4,5,7]:
+                    value_types = json_data["nodes"][el_id]["value_types"]
+                    possible_types = value_types[key]
+                    index = possible_types.index(new_values[key])
+                    ui[key].setCurrentIndex(index)
+                elif el_id in [0,1,2,3,6]:
+                    ui[key].setValue(new_values[key])
+        elif element["is_active"] is False and id_as_string in list(elements_added):
+            fc.removeNode(elements_added[id_as_string])
+            elements_added.pop(id_as_string)
+    fc.outputChanged()
+
+# main
+if __name__ == '__main__':
+    main() # execute main qt event loop
+
